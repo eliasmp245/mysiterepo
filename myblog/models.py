cced0c17d1140abb22d9c3f13054abdb63db6c99
ELIASMP <ELIASMP@L03ELIASMP.pcs.slg.gr>
2021-05-25 11:50:43 +0300

    styleshhet

--- myblog/models.py
+++ myblog/models.py
@@ -52,3 +52,28 @@ def __str__(self):
     return f'Comment by {self.name} on {self.post}'
 
 
+class Post1(models.Model):
+    STATUS_CHOICES = (('draft', 'Draft'),('published', 'Published'),)
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250,unique_for_date='publish')
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    #created  = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager
+    #tags = TaggableManager()
+    def get_absolute_url(self):
+        return reverse('myblog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
+    #class Meta:
+    #ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
